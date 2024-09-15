2d71749b1bbec39135dff06fa4fcb96513728e80
hayapy88 <payagram01@gmail.com>
2024-09-16 00:38:34 +1000

    Integrate nodemon to watch for file changes and change server port

--- server/server.js
+++ server/server.js
@@ -4,6 +4,10 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
+
+app.get("/", (req, res) => {
+  res.send("Test");
+});
 
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
