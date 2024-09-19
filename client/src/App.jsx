bc49d33e229f5729533740f82cee16253bae44b5
hayapy88 <payagram01@gmail.com>
2024-09-20 01:56:18 +1000

    Implement API fetching from frontend and handle CORS issues

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,25 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    async function getAllTodo() {
+      try {
+        const res = await fetch("/api/v1/todo");
+        const todo = await res.json();
+
+        setMessage(todo.message);
+      } catch (error) {
+        console.log("Error fetching data: ", error);
+      }
+    }
+    getAllTodo();
+  }, []);
   return (
     <div>
       <h1>MERN ToDo</h1>
+      {message && <p>{message}</p>}
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 require("dotenv").config();
 
 const app = express();
 const routes = require("./routes/routes");
+
+app.use(cors());
+
 app.use("/api/v1/todo", routes);
 const PORT = process.env.PORT || 3001;
 
