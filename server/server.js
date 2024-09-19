b3fc8a4ffe7faa317fe30e5900dcdfbc3d220bc6
hayapy88 <payagram01@gmail.com>
2024-09-19 23:59:32 +1000

    Change the api path and name to routes

--- server/server.js
+++ server/server.js
@@ -4,8 +4,8 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const app = express();
-const router = require("./routes/routes");
-app.use("/api", router);
+const routes = require("./routes/routes");
+app.use("/api/v1/", routes);
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
