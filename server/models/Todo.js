e1d60700388f8ad4087fa205142e789a6e6f3f07
hayapy88 <payagram01@gmail.com>
2024-09-28 02:44:51 +1000

    Add timestamps for data schema and changed required message

--- server/models/Todo.js
+++ server/models/Todo.js
@@ -1,16 +1,19 @@
 const mongoose = require("mongoose");
 
-const TodoSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: [true, "Please input a title."],
-    trim: true,
-    maxLength: [100, "Title cannot be more than 100 characters."],
+const TodoSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Title is required."],
+      trim: true,
+      maxLength: [100, "Title cannot be more than 100 characters."],
+    },
+    completed: {
+      type: Boolean,
+      default: false,
+    },
   },
-  completed: {
-    type: Boolean,
-    default: false,
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Todo", TodoSchema);
