39b801373dd9e9f3a17b3c3c7fbe9f2d7de5ca78
hayapy88 <payagram01@gmail.com>
2024-09-30 23:59:23 +1000

    Add input field, create button, and checkbox

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,10 @@ function App() {
   const [message, setMessage] = useState("");
   const [todos, setTodos] = useState([]);
 
+  function createTodo() {
+    console.log("Create todo");
+  }
+
   useEffect(() => {
     async function fetchTodos() {
       try {
@@ -23,11 +27,18 @@ function App() {
   return (
     <div className="text-center">
       <h1>MERN ToDo App</h1>
+      <input type="text" className="border" />
+      <button type="submit" className="border" onClick={createTodo}>
+        Create
+      </button>
       <div>
         {todos.map((todo) => {
           return (
             <div key={todo._id}>
-              <input type="checkbox" checked={todo.completed} />
+              <input
+                type="checkbox"
+                className="relative before:content-[''] before:absolute before:-left-7 before:-top-4 before:w-6 before:h-6 before:rounded-full before:border before:border-gray-900 before:bg-white before:cursor-pointer before:checked:bg-blue-500 before:checked:border-transparent before:checked:border-0 before:checked:text-white after:checked:absolute after:checked:-left-6 after:checked:-top-2 after:checked:w-4 after:checked:h-2 after:checked:border-l-2 after:checked:border-b-2 after:checked:border-white after:checked:-rotate-45"
+              />
               {todo.title}
             </div>
           );
