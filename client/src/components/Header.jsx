fb0de00893788174edbc55f9855e91390d4e1686
hayapy88 <payagram01@gmail.com>
2024-10-14 02:05:42 +1100

    Add a subtitle

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,6 +4,7 @@ const Header = () => {
   return (
     <div className="header">
       <h1 className="header__title">MERN Todo App</h1>
+      <p className="header__subtitle">Feel free to create a new Todo!</p>
     </div>
   );
 };
