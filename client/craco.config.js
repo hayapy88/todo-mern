c50ed30ca0df76fce34a6e24ff90da17fa556301
hayapy88 <payagram01@gmail.com>
2024-10-13 23:36:52 +1100

    Fix SCSS compiling issues

--- client/craco.config.js
+++ client/craco.config.js
@@ -1,32 +1,43 @@
+const path = require("path");
+
 module.exports = {
-  webpack: {
-    configure: {
-      module: {
-        rules: [
-          {
-            test: /\.scss$/,
-            use: [
-              "style-loader",
-              "css-loader",
-              {
-                loader: "resolve-url-loader",
-                options: {
-                  sourceMap: true,
-                },
-              },
-              {
-                loader: "sass-loader",
-                options: {
-                  sourceMap: true,
-                  sassOptions: {
-                    includePaths: ["./src/assets/scss"],
-                  },
-                },
-              },
-            ],
-          },
-        ],
+  style: {
+    sass: {
+      loaderOptions: {
+        additionalData: `@import "src/assets/scss/variables.scss";`,
       },
+    },
+    postcss: {
+      plugins: [require("tailwindcss"), require("autoprefixer")],
+    },
+  },
+  webpack: {
+    configure: (webpackConfig, { env, paths }) => {
+      const isProduction = env === "production";
+
+      // Sassのルールを修正
+      const sassRule = webpackConfig.module.rules.find(
+        (rule) => rule.test && rule.test.toString().includes("scss|sass")
+      );
+      if (sassRule) {
+        sassRule.use = sassRule.use.map((loader) => {
+          if (loader.loader && loader.loader.includes("sass-loader")) {
+            return {
+              ...loader,
+              options: {
+                ...loader.options,
+                sourceMap: !isProduction,
+                sassOptions: {
+                  includePaths: [path.resolve(__dirname, "src/assets/scss")],
+                },
+              },
+            };
+          }
+          return loader;
+        });
+      }
+
+      return webpackConfig;
     },
   },
 };
